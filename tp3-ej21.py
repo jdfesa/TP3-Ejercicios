a5a04fef843151ed62e6b520ec975f493d8dffd7
Jose David <asefadoj@gmail.com>
2025-04-13 13:37:52 -0300

    Separando en funciones

--- tp3-ej21.py
+++ tp3-ej21.py
@@ -33,14 +33,16 @@ def ingresar_numero() -> int:
         except ValueError:
             print("Debe ingresar un valor valido!")
 
-def main() -> None: 
-    altura = ingresar_numero()
+def dibujar_triangulo(altura: int) -> None: 
     print(f"Altura = {altura}")
-    
     for i in range(altura, 0, -1):  # Desde altura hasta 1 (decreciendo)
         linea = "+" + ("*" * i) + "+"
         print(linea)
     
+def main() -> None:
+    altura = ingresar_numero()
+    dibujar_triangulo(altura)
+
 if __name__ == "__main__":
     main()
 
